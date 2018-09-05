a506584ac3b8397f90a23b3a461dd0b6ce49211d
wosiu2 <wosiu2@go2.pl>
2018-09-05 18:19:41 +0200

    #1# dodano narzedzie do odleglosci pomiedzy dwoma pkt na podstawie szerokosci ii wysokosci geograficznej, kąta rotacji oraz transformacji obiektu strzalki

---
+++ TourHelper.Base/Logic/IGeolocationTool.cs
@@ -0,0 +1,14 @@
+﻿using TourHelper.Base.Model.Entity;
+using UnityEngine;
+
+
+namespace TourHelper.Base.Logic
+{
+    interface IGeolocationTool
+    {
+        double Bearing(Coordinates coor);
+        double Distance(Coordinates coor);
+        double RotationAngle(Coordinates coor);
+        void Transform(Transform obj);
+    }
+}
