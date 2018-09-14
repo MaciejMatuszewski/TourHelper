68658d61e52ae7efa63b9ab26f8207deb8d7d028
wosiu2 <wosiu2@go2.pl>
2018-09-14 13:34:09 +0200

    #1# dodano pkt testowe

--- TourHelper/Assets/Scripts/ButtonClick.cs
+++ TourHelper/Assets/Scripts/ButtonClick.cs
@@ -16,19 +16,26 @@ public class ButtonClick : MonoBehaviour {
         origin.Latitude = 52.463645f;
         origin.Longitude = 16.921922f;
 
-        Coordinates[] c = new Coordinates[2];
+        Coordinates[] c = new Coordinates[3];
 
+
+        //-----------------
+        Coordinates price0 = new Coordinates();
+        price0.Latitude = 52.463645f;
+        price0.Longitude = 16.921922f;
+        c[0] = price0;
+        //52.463812, 16.921077
         //-----------------
         Coordinates price1 = new Coordinates();
         price1.Latitude = 52.463812f;
         price1.Longitude = 16.921077f;
-        c[0] = price1;
+        c[1] = price1;
         //52.463812, 16.921077
         //-------------------------------
         Coordinates price2 = new Coordinates();
         price2.Latitude = 52.463554f;
         price2.Longitude = 16.921101f;
-        c[1] = price2;
+        c[2] = price2;
         //52.463554, 16.921101
         //-----------------
 
--- TourHelper/Assets/Scripts/PhoneCamera.cs
+++ TourHelper/Assets/Scripts/PhoneCamera.cs
@@ -2,39 +2,42 @@
 using TourHelper.Manager.Devices;
 using UnityEngine.UI;
 
-public class PhoneCamera : MonoBehaviour {
+public class PhoneCamera : MonoBehaviour
+{
     private CameraManager camera;
     public RawImage background;
-    
-   // public AspectRatioFitter fit;
+
+    public AspectRatioFitter fit;
 
 
-	// Use this for initialization
-	private void Start () {
+    // Use this for initialization
+    private void Start()
+    {
         camera = CameraManager.Instance;
         StartCoroutine(camera.StartService(10));
-        
+
         //background.texture = camera.GetScreen();
     }
-	
-	// Update is called once per frame
-	private void Update () {
-	    if(!camera.IsReady())
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (!camera.IsReady())
         {
             return;
         }
 
         background.texture = camera.GetScreen();
-       // float ratio = (float)(camera.GetScreen().width / (float)camera.GetScreen().height);
-        //fit.aspectRatio = ratio;
+        float ratio = (float)(camera.GetScreen().width / (float)camera.GetScreen().height);
+        fit.aspectRatio = ratio;
 
         float scaleY = camera.GetScreen().videoVerticallyMirrored ? -1f : 1f;
-               
+
 
         background.rectTransform.localScale = new Vector3(1f, scaleY, 1f);
 
         int orient = -camera.GetScreen().videoRotationAngle;
-        background.rectTransform.localEulerAngles= new Vector3(0, 0, orient);
+        background.rectTransform.localEulerAngles = new Vector3(0, 0, orient);
 
     }
 }
