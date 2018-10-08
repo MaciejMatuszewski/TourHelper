e7aaade878ce45122e40ac99776ea02d01908f4d
wosiu2 <wosiu2@go2.pl>
2018-10-08 14:15:30 +0200

    #1# Ukonczony panel testowania algorytmu

--- TourHelper/Assets/Scripts/Acceleration.cs
+++ TourHelper/Assets/Scripts/Acceleration.cs
@@ -13,8 +13,10 @@ using UnityEngine.UI;
 
 public class Acceleration : MonoBehaviour
 {
+    public bool isOn;
+
     private GameObject cameraContainer;
-    public Text text;
+    public InputField _lat,_lon,_gpsErr,_accErr,_path;
     private AccelerationManager acc;
     private GyroManager g;
     private Coordinates origin;
@@ -36,6 +38,13 @@ public class Acceleration : MonoBehaviour
     void Start()
     {
 
+        
+
+        _lat.text = "52.463907";
+        _lon.text = "16.920955";
+        _gpsErr.text = "1.0";
+        _accErr.text = "1.0";
+        _path.text = "Log1.txt";
 
         //TRANSLATOR WSPOLRZEDNYCH
         origin = new Coordinates();
@@ -57,14 +66,40 @@ public class Acceleration : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (isOn)
+        {
+            p.Filter.DeltaTime = Time.deltaTime;
 
-        p.Filter.DeltaTime = Time.deltaTime;
+            v = p.GetPosition();
+        }
 
-        v=p.GetPosition();
 
-        text.text = v.x.ToString() + "," + v.y.ToString() + "," + v.z.ToString();
+       // text.text = v.x.ToString() + "," + v.y.ToString() + "," + v.z.ToString();
         //text.text = p.Filter.InitialPosition.GetByIndex(0,0)+" "+p.Filter.InitialPosition.GetByIndex(1, 0);
         
 
+    }
+
+
+    public void OnStartButtonClick(Text t)
+    {
+        if (!isOn)
+        {
+            isOn = true;
+            t.text = "STOP";
+            origin = new Coordinates();
+            origin.Latitude = (float)Convert.ToDouble(_lat.text);
+            origin.Longitude =(float)Convert.ToDouble(_lon.text);
+            p.Origin = origin;
+            p.Filter.GPSError = Convert.ToDouble(_gpsErr.text);
+            p.Filter.AccelerationError = Convert.ToDouble(_accErr.text);
+
+            p.LogPath = Application.persistentDataPath + _path.text;
+        }
+        else
+        {
+            isOn = false;
+            t.text = "START";
+        }
     }
 }
