eead288a536ccd18c9fc8546c7a37dd1d52f29a7
wosiu2 <wosiu2@go2.pl>
2018-10-23 17:38:05 +0200

    #1#Dodano animowane panele
    #2# Dodano Eventy do przyciskow

---
+++ TourHelper/Assets/Scripts/ElementsManagement/PanelEvent.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelEvent : MonoBehaviour {
+
+    private float dt = 0;
+    private Vector3 _position;
+    private Vector3 _initialPosition;
+    private Vector3 _frontPosition;
+    public bool inProcess = false;
+
+    public int speed ;
+    public bool isVisible = false;
+
+
+	// Use this for initialization
+	void Start () {
+        _initialPosition = transform.localPosition;
+        _frontPosition = new Vector3(0, 0, 0);
+        _position = transform.localPosition;
+        speed = 1;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (inProcess)
+        {
+            dt += speed * Time.deltaTime;
+            if (isVisible)
+            {
+                inProcess = ToBack();
+            }
+            else
+            {
+                inProcess = ToFront();
+            }
+        }
+    }
+
+    public void MovePanel()
+    {
+        inProcess = true;
+
+    }
+
+    private bool ToBack()
+    {
+
+        _position = Vector3.Lerp(_frontPosition, _initialPosition, dt);
+
+        transform.localPosition = _position;
+
+
+        var diff = _initialPosition - _position;
+        if (Math.Abs(diff.x) < 0.001 && Math.Abs(diff.y) < 0.001 && Math.Abs(diff.z) < 0.001)
+        {
+            dt = 0;
+            isVisible = false;
+            return false;
+        }
+        return true;
+    }
+
+    private bool ToFront()
+    {
+        _position = Vector3.Lerp(_initialPosition, _frontPosition, dt);
+
+        transform.localPosition = _position;
+
+        var diff = _frontPosition - _position;
+        if (Math.Abs(diff.x) < 0.001&& Math.Abs(diff.y) < 0.001&& Math.Abs(diff.z) < 0.001)
+        {
+            dt = 0;
+            isVisible = true;
+            return false;
+        }
+        return true;
+    }
+
+}
--- TourHelper/Assets/Scripts/GUIManager.cs
+++ TourHelper/Assets/Scripts/GUIManager.cs
@@ -4,24 +4,41 @@ using TourHelper.Manager.Devices;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class GUIManager : MonoBehaviour {
+public class GUIManager : MonoBehaviour
+{
     public bool menuDown = false;
+
+    public bool mapInProcess = false;
+
+
     public Image gpsOn;
     public Image gpsOff;
 
+
+    private Transform mapPanel;
     private IGpsManager _gps;
 
     private void Start()
     {
         _gps = GpsManager.Instance;
+
+        float aspect = (float)Screen.width / ((float)Screen.height);
+
+        Component[] fitters = GetComponentsInChildren(typeof(AspectRatioFitter),true);
+
+        foreach (Component fitter in fitters)
+        {
+            ((AspectRatioFitter)fitter).aspectRatio = aspect;
+        }
+        
     }
 
     private void Update()
     {
         if (_gps.IsEnabled())
         {
-            gpsOn.enabled=true;
-            gpsOff.enabled=false;
+            gpsOn.enabled = true;
+            gpsOff.enabled = false;
         }
         else
         {
@@ -31,18 +48,32 @@ public class GUIManager : MonoBehaviour {
 
     }
 
-    public void OnSettingClick(RectTransform panel)
+    public void OnSettingClickDropDown(RectTransform panel)
     {
         if (menuDown)
         {
-            panel.sizeDelta= new Vector2(panel.sizeDelta.x, 270);
+            panel.sizeDelta = new Vector2(panel.sizeDelta.x, 270);
             menuDown = false;
         }
         else
         {
-            panel.sizeDelta = new Vector2(panel.sizeDelta.x, 800);
+            panel.sizeDelta = new Vector2(panel.sizeDelta.x, 700);
             menuDown = true;
         }
     }
+
+    public void OnSettingClickMenu(GameObject dropDown)
+    {
+        if (menuDown)
+        {
+            dropDown.SetActive(true);
+        }
+        else
+        {
+            dropDown.SetActive(false);
+        }
+    }
+
+
 
 }
