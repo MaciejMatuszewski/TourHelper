140bc7a99e66b80da4fc3bf0e2f5e0054856b7e2
wosiu2 <wosiu2@go2.pl>
2018-11-16 16:48:38 +0100

    #1# Dodano updane info o pkt i wycieczne na glownym panelu

---
+++ TourHelper/Assets/Scripts/ElementsManagement/BottomPanelEvent.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TourHelper.Repository;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BottomPanelEvent : MonoBehaviour {
+
+    public Text tour;
+    public Text point;
+    private int lastTourID;
+
+	void Update () {
+
+        if (lastTourID!= PlayerPrefs.GetInt("TourID"))
+        {
+            var tourRepo = new TourRepository();
+            var tourEnt = tourRepo.Get(PlayerPrefs.GetInt("TourID"));
+
+            if (tour != null)
+            {
+                tour.text = tourEnt.Name;
+            }
+            else
+            {
+                tour.text = "";
+            }
+            lastTourID = PlayerPrefs.GetInt("TourID");
+        }
+
+        point.text = PlayerPrefs.GetString("PointName");
+
+    }
+}
